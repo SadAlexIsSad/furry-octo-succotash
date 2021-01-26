223646a97fb6f88c697b9f0df5695d7c77c646cd
SadAlexIsSad <70234971+SadAlexIsSad@users.noreply.github.com>
2021-01-26 15:18:18 -0500

    Add files via upload

---
+++ ImOnlyHappyOnTheInside-HelloWorld.cpp
@@ -0,0 +1,12 @@
+﻿//Alex R
+// I have deep rooted emotional issues due to Oprah being cancelled
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+	cout <<	"Hello Darkness My Old Friend!" << endl << endl;
+	cout << "Snape Kills Dumbeldore" << endl;
+	system("pause");
+	return 0;
+}
